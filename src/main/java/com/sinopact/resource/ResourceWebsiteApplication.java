a6427f48fe4cf08a9bc11c928f07beec518ac280
Freeman Hu <820220700@qq.com>
2020-12-08 16:13:29 +0800

    initialize the project

---
+++ src/main/java/com/sinopact/resource/ResourceWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.sinopact.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sinopact/resource/ResourceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinopact.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
